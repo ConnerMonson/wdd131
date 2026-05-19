7c0ea6d0e5357287e64ffe6fcf20d69ea278cb2b
ConnerMonson <connerrmonson@gmail.com>
2026-05-19 12:38:21 -0500

    continued to fix

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+// Current Year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+// Last Modified
+const lastModified = document.lastModified
+document.getElementById("lastModified").innerHTML = lastModified;
